709ba67ec0adcf4a5dec0cc2445d3a843cff93b2
Luk14 <lukaszplayz121@gmail.com>
2021-01-21 10:20:11 +0000

    Added Base Porject Files + Document Folder

---
+++ Player-Data-Manager/src/test/java/me/lukasz/PlayerDataManagerApplicationTests.java
@@ -0,0 +1,15 @@
+package me.lukasz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerDataManagerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
