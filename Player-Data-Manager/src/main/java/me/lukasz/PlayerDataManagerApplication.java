813fcbed1a6418493a4c1d6760a7fee226b106a8
Luk14 <lukaszplayz121@gmail.com>
2021-01-21 10:20:11 +0000

    Added Base Porject Files + Document Folder

---
+++ Player-Data-Manager/src/main/java/me/lukasz/PlayerDataManagerApplication.java
@@ -0,0 +1,15 @@
+package me.lukasz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerDataManagerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PlayerDataManagerApplication.class, args);
+    }
+
+}
