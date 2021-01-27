cec9066cfdf5b66514fb068ada619d0334e5a669
Luk14 <lukaszplayz121@gmail.com>
2021-01-27 14:37:34 +0000

    [DMS-25] Added Structure to User & Statistics Domain

--- Player-Data-Manager/src/main/java/me/lukasz/presistance/domain/Statistics.java
+++ Player-Data-Manager/src/main/java/me/lukasz/presistance/domain/Statistics.java
@@ -1,5 +1,25 @@
 package me.lukasz.presistance.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Statistics
 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int blocks_walked;
+    private int blocks_broken;
+    private int kills;
+    private int deaths;
+    @OneToOne
+    private User user;
 }
--- Player-Data-Manager/src/main/java/me/lukasz/presistance/domain/User.java
+++ Player-Data-Manager/src/main/java/me/lukasz/presistance/domain/User.java
@@ -1,5 +1,29 @@
 package me.lukasz.presistance.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User
 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String fname;
+    private String lname;
+    private String email;
+    @OneToOne(mappedBy = "user", fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Statistics statistics;
+
 }
