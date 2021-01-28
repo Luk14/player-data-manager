c5f2bda8c282098648760acc3ceb30e73eadd61e
Luk14 <lukaszplayz121@gmail.com>
2021-01-28 10:42:45 +0000

    [DMS-49] Added Read Controller Actions

--- Player-Data-Manager/src/main/java/me/lukasz/rest/CarController.java
+++ Player-Data-Manager/src/main/java/me/lukasz/rest/CarController.java
@@ -1,5 +1,41 @@
 package me.lukasz.rest;
 
+import me.lukasz.presistance.dto.CarDTO;
+import me.lukasz.services.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("car")
 public class CarController
 {
+
+    private CarService carService;
+
+    @Autowired
+    public CarController(CarService carService)
+    {
+        this.carService = carService;
+    }
+
+    @GetMapping("/readall")
+    public ResponseEntity<List<CarDTO>> readAll()
+    {
+        return ResponseEntity.ok(carService.readAll());
+    }
+
+    @GetMapping("/read/{id}")
+    public ResponseEntity<CarDTO> read(@PathVariable("id") int id)
+    {
+        return ResponseEntity.ok(carService.getCar(id));
+    }
+
+    
+
 }
